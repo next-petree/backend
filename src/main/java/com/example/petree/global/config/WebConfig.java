a25a7d6824f08993dc501fdc9b4a407ee1c7f643
suhyun <qkrtngus116@naver.com>
2024-01-05 16:37:37 +0900

    fix: cors에러 해결을 위한 주소 변경 및 PATCH 메소드 허용 추가

--- src/main/java/com/example/petree/global/config/WebConfig.java
+++ src/main/java/com/example/petree/global/config/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig implements WebMvcConfigurer{
                 .addMapping("/**") // 프로그램에서 제공하는 URL
                 .allowedOrigins("http://localhost:3000", "https://www.nextpetree.store", "https://nextpetree.store") // 청을 허용할 출처를 명시, 전체 허용 (가능하다면 목록을 작성한다.
                 .allowedHeaders("*") // 어떤 헤더들을 허용할 것인지
-                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
+                .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowCredentials(true); // 쿠키 요청을 허용한다(다른 도메인 서버에 인증하는 경우에만 사용해야하며, true 설정시 보안상 이슈가 발생할 수 있다)
         // .maxAge(1500) // preflight 요청에 대한 응답을 브라우저에서 캐싱하는 시간 ;
     }
